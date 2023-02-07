1d3535cb15019187211c90332b870bff3b31439c
Muhammed Muhtarov <muhammed1410@yandex.ru>
2023-02-07 18:37:57 +0300

    server status

--- server/routes.go
+++ server/routes.go
@@ -12,5 +12,8 @@ func routes() http.Handler {
 	app.Get("/", http.HandlerFunc(handlers.Home))
 	app.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	app.Get("/static/", http.StripPrefix("/static", fileServer))
+
 	return app
 }
